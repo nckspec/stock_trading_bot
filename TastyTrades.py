32499eae05aca3b398c7849f22dbd0ded5654c59
NSpector <nspectorit@gmail.com>
2023-07-15 16:48:53 -0700

    Created get_option_contract() which allows us to get a verified symbol for an option contract if we specify the strike price, expiration date, and type (put/call)

--- TastyTrades.py
+++ TastyTrades.py
@@ -1,6 +1,8 @@
 import logging
 import requests
-my_logger = logging.getLogger('logger')
+import datetime
+import pytz
+LOGGER = logging.getLogger('logger')
 
 API_URL = "https://api.tastyworks.com"
 SANDBOX_API_URL = "https://api.tastyworks.com"
@@ -10,6 +12,13 @@ class TastyTrades:
 
     def __init__(self, username, password, account_id, debug=False):
         try:
+            self._logger = logging.LoggerAdapter(LOGGER,
+                                   {"extra":
+                                    {'username': username,
+                                    'password': password,
+                                    'account_id': account_id,
+                                    'debug': str(debug)}})
+
             if debug:
                 self._api_url = API_URL
             else:
@@ -21,10 +30,14 @@ class TastyTrades:
             #  with all future requests
             self._session_token = self._connect(username, password)
 
+            self._logger.debug(f"Received session token = {self._session_token}", extra={"session_token": self._session_token})
+
             self._headers = {
                 'User-agent': 'Mozilla/5.0',
                 'Authorization': self._session_token
             }
+
+            self._logger.debug("Initiated TastyTrades() object")
 
         except Exception as ex:
             message = f"Error in TastyTrades.__init__(): {ex}"
@@ -39,6 +52,10 @@ class TastyTrades:
                 "login": username,
                 "password": password
             }
+
+            self._logger.info("Attempting to connect to Tasty Trades with "
+                              "username: {username}/account id: {self._account_id}")
+
             response = requests.post(self._api_url + endpoint, headers=USER_AGENT, json=json)
 
             #  If the request resulted in an error raise it
@@ -46,6 +63,8 @@ class TastyTrades:
                 message = f"Error connecting to Tasty Trades: status code ({response.status_code}): " \
                           f"{response.json() if response.headers.get('content-type') == 'application/json' else 'no response'}"
                 raise Exception(message)
+
+            self._logger.info(f"Connected to TastyTrades with username: {username}/account id: {self._account_id}")
 
             #  Return the session token to be used with future connections
             return response.json()['data']['session-token']
@@ -56,18 +75,33 @@ class TastyTrades:
 
     def _make_request(self, endpoint, type="get", data=dict()):
         try:
-            if type is "get":
+            self._logger.debug(f"Entering TastyTrades._make_request()\n"
+                               f"endpoint: {endpoint}\n"
+                               f"type: {type}\n"
+                               f"data: {data}", extra={"endpoint": endpoint,
+                                                       "type": type,
+                                                       "data": data})
+
+            type = type.lower()
+
+            if type == "get":
                 response = requests.get(self._api_url + endpoint, headers=self._headers)
                 if response.status_code != 200:
                     message = f"Error connecting to Tasty Trades: status code ({response.status_code}): " \
                               f"{response.json() if response.headers.get('content-type') == 'application/json' else 'no response'}"
                     raise Exception(message)
-            elif type is "post":
+            elif type == "post":
                 response = requests.post(self._api_url + endpoint, headers=self._headers, json=data)
                 if response.status_code != 201:
                     message = f"Error connecting to Tasty Trades: status code ({response.status_code}): " \
                               f"{response.json() if response.headers.get('content-type') == 'application/json' else 'no response'}"
                     raise Exception(message)
+
+            self._logger.debug(f"Exiting TastyTrades._make_request()\n"
+                               f"status code: {response.status_code}\n"
+                               f"json: {response.json()}", extra={"endpoint": endpoint,
+                                                       "type": type,
+                                                       "data": data})
 
             return response
 
@@ -85,9 +119,73 @@ class TastyTrades:
 
             response = self._make_request(endpoint, "get")
 
-            return response.json()['data']['cash-balance']
+            balance = float(response.json()['data']['cash-balance'])
+
+            self._logger.info(f"Retrieved balance of ${balance} on account id: {self._account_id}")
+
+            return balance
 
         except Exception as ex:
             message = f"Error in TastyTrades.get_balance(): {ex}"
             raise Exception(message)
+
+    def get_option_contract(self, symbol: str, type: str, strike_price: float, expiration_date: datetime.date):
+        try:
+            self._logger.debug("Entering TastyTrades.get_option_contract()\n"
+                               f"symbol: {symbol}\n"
+                               f"type: {type}\n"
+                               f"strike_price: {strike_price}\n"
+                               f"expiration_date: {expiration_date}")
+
+            type = type.lower()
+
+            if type == "call" or type == 'c':
+                type = 'C'
+            elif type == "put" or type == 'p':
+                type = 'P'
+            else:
+                raise Exception("The type parameter must be either 'call' or 'put'")
+
+            #  Concatenate the symbol in ooc format yymmddTypeStrikePrice
+            #  ex. 230717P10700000 - July 17, 2023, Put, Strike price
+            ooc_symbol = symbol + "  " + self.convert_date_to_ooc(expiration_date) + type + self.convert_strike_price_to_ooc(strike_price)
+
+            response = self._make_request(f"/instruments/equity-options/{ooc_symbol}", "get")
+
+            self._logger.debug("Exiting TastyTrades.get_option_contract()\n"
+                               f"ooc_symbol: {ooc_symbol}\n")
+
+            return ooc_symbol, response
+
+        except Exception as ex:
+            message = f"Error in TastyTrades.get_option_contract(): {ex}"
+            raise Exception(message)
+
+    #  Will convert the date to ooc format. Must input a date() object and
+    #  will return a string in ooc format
+    def convert_date_to_ooc(self, date: datetime.date):
+        self._logger.debug("Entering TastyTrades.convert_date_to_ooc()\n"
+                           f"date: {date}")
+
+        date_format = "%y%m%d"
+        date = date.strftime(date_format)
+
+        self._logger.debug("Exiting TastyTrades.convert_date_to_ooc()\n"
+                           f"date: {date}")
+
+        return date
+
+    #  Multiplies the price by 1000 and then pads the number with
+    #  leading 0s
+    def convert_strike_price_to_ooc(self, strike_price: float):
+        self._logger.debug("Entering TastyTrades.convert_strike_price_to_ooc()\n"
+                           f"strike_price: {strike_price}")
+
+        ooc_format = int(strike_price * 1000)
+        ooc_format = f"{ooc_format:08}"
+
+        self._logger.debug("Exiting TastyTrades.convert_strike_price_to_ooc()\n"
+                           f"ooc_format: {ooc_format}")
+
+        return ooc_format
 
--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import logging
 import TastyTrades
+import datetime
+import pytz
 import requests
 import time
 
 LOGGER = logging.getLogger('logger')
 LOGGER.setLevel(logging.DEBUG)
 
-console_handler = logging.StreamHandler()
-LOGGER.addHandler(console_handler)
 
-LOGGER.debug('Hello Graylog.')
+FORMAT = '%(asctime)s - %(levelname)s - %(message)s\n%(extra)s\n'
+logging.basicConfig(format=FORMAT, datefmt='%m/%d/%Y %H:%M:%S')
+# logging.disable(logging.DEBUG)
+
+# LOGGER.debug('Hello Graylog.')
 
 tasty = TastyTrades.TastyTrades(username="nckspec", password="1Success2015!", account_id="5WX92378")
 
 print(tasty.get_balance())
 
-try:
-    response = tasty._make_request("/instruments/equity-options/NDXP  230714P15700000", "get")
-    print(response.json())
-except Exception as ex:
-    print(ex)
+#  Get the current date in Los Angeles time
+# date = pytz.utc.localize(datetime.datetime.utcnow())
+# date = date.astimezone(pytz.timezone("America/Los_Angeles"))
+
+date = datetime.date(2023, 7, 17)
+
+ooc, response = tasty.get_option_contract("NDXP", "Put", 10700, date)
+
+print(ooc)
+print(response.json())
+
+
+
+# try:
+#     response = tasty._make_request("/instruments/equity-options/NDXP  230714P15700000", "get")
+#     print(response.json())
+# except Exception as ex:
+#     print(ex)
 
 # response = tasty._make_request("/option-chains/NDX/compact", "get")
 #
